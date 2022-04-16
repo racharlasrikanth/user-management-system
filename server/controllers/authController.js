bd4f804011e6e87d73c33682f47a9d676311fe31
Srikanth <sri1@publicisgroupe.net>
2022-04-16 16:36:29 +0530

    register user controller added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,9 +5,42 @@ const CustomError = require('./../errors');
 const {
     attachCookiesToResponse
 } = require('./../utils');
+const crypto = require('crypto');
 
 const register = async (req, res) => {
-    res.send('register user');
+
+    const { email, name, password } = req.body;
+
+    if (!email || !name || !password) {
+        throw new CustomError.BadRequestError('Please provide email, name and password');
+    }
+
+    const emailAlreadyExists = await User.findOne({ email });
+    if (emailAlreadyExists) {
+        throw new CustomError.BadRequestError('Email already exists');
+    }
+
+    // first registered user is admin
+    const isFirstUser = await User.countDocuments({}) === 0;
+    const role = isFirstUser ? "admin" : "user";
+
+    const verificationToken = crypto.randomBytes(40).toString('hex');
+
+    const user = await User.create({
+        name, email, password, role, verificationToken
+    })
+
+    const origin = process.env.FRONT_END_ORIGIN;
+
+    // send email
+    // code.....send email
+
+
+    // send verification token back only while testing in postman
+    res.status(StatusCodes.CREATED).json({
+        user,
+        message: "Success, Please check your email to verify account"
+    })
 }
 
 const verifyEmail = async (req, res) => {
