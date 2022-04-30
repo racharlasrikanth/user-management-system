ebfb0762255fbf6ee1f1f8ab327f7a962daf0ec4
Srikanth <sri1@publicisgroupe.net>
2022-04-30 15:25:47 +0530

    navbar styles added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import logo from "./../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { useGlobalContext } from "./../context";
 
 const Navbar = () => {
+
+  // fetch values from context
+  const { user, logoutUser } = useGlobalContext();
+
   return (
-    <div>Navbar</div>
+    <Wrapper>
+      <div className="nav-center">
+        <Link to="/" className='home-link'>
+          <img src={logo} alt="logo" className='logo' />
+        </Link>
+        {
+          user && (
+            <div className="nav-links">
+              <p>hello, <span>{user?.name || "no-username"}</span></p>
+              <button className="btn btn-small" onClick={() => {
+                logoutUser();
+              }}>
+                logout
+              </button>
+            </div>
+          )
+        }
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.nav`
+  background: var(--white);
+  height: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .nav-center {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  .nav-links {
+    display: flex;
+    flex-direction: column;
+  }
+  .nav-links p {
+    margin: 0px;
+    text-transform: capitalize;
+    margin-bottom: .25rem;
+    span {
+      font-weight: bold;
+      letter-spacing: 1px;
+    }
+  }
+  .home-link {
+    display: flex;
+    align-items: flex-end;
+  }
+  @media (min-width: 767px) {
+    .nav-links {
+      flex-direction: row;
+      align-items: center;
+    }
+    .nav-links p {
+      margin: 0px;
+      margin-right: 1.5rem;
+    }
+  }
+`
 
 export default Navbar;
