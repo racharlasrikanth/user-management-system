0c7867781b8e482d61957488898b3076a28b4f3b
Srikanth <sri1@publicisgroupe.net>
2022-05-01 12:16:52 +0530

    verify email code added

--- client/src/pages/Verify.js
+++ client/src/pages/Verify.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useLocation, Link } from "react-router-dom";
+import { useGlobalContext } from "./../context";
+import axios from 'axios';
+import styled from "styled-components";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const Verify = () => {
+
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { isLoading } = useGlobalContext();
+  const query = useQuery();
+
+  const verifyToken = async () => {
+    setLoading(true);
+    try {
+      await axios.post("/api/v1/auth/verify-email", {
+        verificationToken: query.get('token'),
+        email: query.get('email')
+      })
+    } catch (error) {
+      setError(true)
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    if (!isLoading) {
+      verifyToken();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  
+
+  if (loading) {
+    return (
+      <Wrapper className='page'>
+        <h2>Loading...</h2>
+      </Wrapper>
+    )
+  }
+
+  if (error) {
+    return (
+      <Wrapper className='page'>
+        <h4>There was an error, please double check your verification link</h4>
+      </Wrapper>
+    )
+  }
+
   return (
-    <div>Verify</div>
+    <Wrapper>
+      <h2>Account Confirmed</h2>
+      <Link to="/login" className='btn'>Please Login</Link>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  
+`
 
 export default Verify;
