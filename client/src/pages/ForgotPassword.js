0c545026b954fedb121f153c1bac8216eeeb8de0
Srikanth <sri1@publicisgroupe.net>
2022-05-01 13:51:44 +0530

    reset and forgot pasword added

--- client/src/pages/ForgotPassword.js
+++ client/src/pages/ForgotPassword.js
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, {useState} from 'react';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import FormRow from "./../components/FormRow";
+import axios from 'axios';
+import useLocalState from "./../utils/localState";
 
 const ForgotPassword = () => {
+
+  const [email, setEmail] = useState('');
+  const {
+    alert,
+    showAlert,
+    loading,
+    setLoading,
+    success,
+    setSuccess,
+    hideAlert
+  } = useLocalState();
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    hideAlert();
+    if (!email) {
+      showAlert({
+        text: "Please provide email"
+      })
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const {data} = await axios.post("/api/v1/auth/forgot-password", {
+        email
+      });
+      console.log(data);
+      showAlert({
+        text: data.message,
+        type: 'success'
+      })
+      setSuccess(true);
+    } catch (error) {
+      showAlert({
+        text:"something went wrong, please try again",
+      })
+      setSuccess(true);
+    }
+    setLoading(false)
+  };
+
   return (
-    <div>ForgotPassword</div>
+    <Wrapper className='page'>
+      {alert.show && (
+        <div className={`alert alert-${alert.type}`}>{alert.text}</div>
+      )}
+      {
+        !success && (
+          <form
+            className={loading ? "form form-loading" : "form"}
+            onSubmit={handleSubmit}
+          >
+            <h4>Forgot Password</h4>
+
+            {/* single form row */}
+            <FormRow
+              type="email"
+              name="email"
+              value={email}
+              handleChange={handleChange}
+            />
+
+            <button type="submit" className='btn btn-block' disabled={loading}>
+              {loading ? "Please wait..." : "Get Reset Password Link"}
+            </button>
+            <p>
+              Already a have an account?
+              <Link to='/login' className='login-link'>
+                Log In
+              </Link>
+            </p>
+          </form>
+        )
+      }
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.main`
+  h4,
+  p {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+  }
+  .login-link {
+    display: inline-block;
+    margin-left: 0.25rem;
+    text-transform: capitalize;
+    color: var(--primary-500);
+    cursor: pointer;
+  }
+`;
 
 export default ForgotPassword;
--- client/src/pages/ResetPassword.js
+++ client/src/pages/ResetPassword.js
@@ -1,9 +1,109 @@
-import React from 'react'
+import React, {useState} from 'react';
+import styled from "styled-components";
+import { useNavigate, useLocation } from "react-router-dom";
+import FormRow from "./../components/FormRow";
+import axios from 'axios';
+import useLocalState from "./../utils/localState";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const ResetPassword = () => {
+  const navigate = useNavigate();
+  const [password, setPassword] = useState('');
+  const {
+    alert,
+    showAlert,
+    loading,
+    setLoading,
+    success,
+    setSuccess,
+    hideAlert
+  } = useLocalState();
+
+  const query = useQuery();
+
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    hideAlert();
+    if (!password) {
+      showAlert({
+        text: "Please provide password"
+      })
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const {data} = await axios.post("/api/v1/auth/reset-password", {
+        password,
+        token: query.get('token'),
+        email: query.get('email')
+      });
+      console.log(data);
+      showAlert({
+        text: data.message,
+        type: 'success'
+      })
+      setSuccess(true);
+      setTimeout(() => {
+        navigate('/login');
+      }, 3000);
+    } catch (error) {
+      showAlert({
+        text: error.response.data.message,
+      })
+      setSuccess(true);
+    }
+    setLoading(false)
+  };
+
   return (
-    <div>ResetPassword</div>
+    <Wrapper className='page'>
+      {alert.show && (
+        <div className={`alert alert-${alert.type}`}>{alert.text}</div>
+      )}
+      {
+        !success && (
+          <form
+            className={loading ? "form form-loading" : "form"}
+            onSubmit={handleSubmit}
+          >
+            <h4>Reset Password</h4>
+
+            {/* single form row */}
+            <FormRow
+              type="password"
+              name="password"
+              value={password}
+              handleChange={handleChange}
+            />
+
+            <button type="submit" className='btn btn-block' disabled={loading}>
+              {loading ? "Please wait..." : "Get Reset Password Link"}
+            </button>
+          </form>
+        )
+      }
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  h4,
+  p {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+  }
+`;
 
 export default ResetPassword;
